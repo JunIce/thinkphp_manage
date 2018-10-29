5a0d506cec4f4223af633f304949a6a46dd37d2a
vols <east_qiu@outlook.com>
2018-10-29 23:32:27 +0800

    new

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -6,7 +6,41 @@ class Index extends \think\Controller
 {
     public function index()
     {
-        return view('index');
+        $data = [
+            [
+                'username' => 'David Grey',
+                'userpic' => "images/faces/face1.jpg",
+                'text' => 'Fund is not recieved',
+                'update_at' => 'Dec 5, 2017',
+                'status' => 1,
+                'tracking_id' => 'WD-12345'
+            ],
+            [
+                'username' => 'Stella Johnson',
+                'userpic' => "images/faces/face2.jpg",
+                'text' => 'High loading time',
+                'update_at' => 'Dec 5, 2017',
+                'status' => 1,
+                'tracking_id' => 'WD-12345'
+            ],
+            [
+                'username' => 'Marina Michel ',
+                'userpic' => "images/faces/face3.jpg",
+                'text' => 'High loading time',
+                'update_at' => 'Dec 5, 2017',
+                'status' => 1,
+                'tracking_id' => 'WD-12345'
+            ],
+            [
+                'username' => ' John Doe ',
+                'userpic' => "images/faces/face4.jpg",
+                'text' => 'High loading time',
+                'update_at' => 'Dec 5, 2017',
+                'status' => 1,
+                'tracking_id' => 'WD-12345'
+            ]
+        ];
+        return view('index', ['data' => $data]);
     }
 
     public function button(){
